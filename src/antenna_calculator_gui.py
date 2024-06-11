1e67c3322a16072a7f44209b65a263b94342bae9
LC_Linkous <L.C.Linkous@gmail.com>
2024-06-11 12:05:47 -0400

    gui addition

--- src/antenna_calculator_gui.py
+++ src/antenna_calculator_gui.py
@@ -1,1 +1,17 @@
 #! /usr/bin/python3
+
+import wx
+import wx.lib.mixins.inspection as wit
+from gui_frame import GFrame
+
+
+def main():
+ 
+        app = wit.InspectableApp()
+        GUIFrame = GFrame(None, title='Antenna Calculator GUI')
+        GUIFrame.Show()
+        app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
